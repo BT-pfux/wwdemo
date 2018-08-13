8852dbbd8bd7d5f191f50bf1308654be5e1ec4d3
ccebrian <carlos.cebrian-sanchez@braintec-group.com>
2018-08-13 12:00:38 +0200

    [t9974] ADD checkbox in second page and redirection to signature

--- wwwizard/models/sale_order_ext.py
+++ wwwizard/models/sale_order_ext.py
@@ -5,7 +5,7 @@ from odoo.http import request
 class WWSaleOrderExt(models.Model):
     _inherit = "sale.order"
 
-    #ww_insurance_ids = fields.One2many("ww.insurance", "sale_order_id")
+    ww_insurance_id = fields.Many2one("ww.insurance")
 
     @api.model
     def create(self, vals):
@@ -20,22 +20,20 @@ class WWSaleOrderExt(models.Model):
             'uom_id': self.env.ref('product.product_uom_unit').id,
             'uom_po_id': self.env.ref('product.product_uom_unit').id,
         })
-        partner_id = self.env.user.partner_id.id
-        vals.update({'partner_id': partner_id,
-                     'state': 'sent',
+        vals.update({'state': 'draft',
                      'order_line': [],
                      'payment_tx_ids': [],
                      'activity_ids': [],
-                     'website_order_line': [],
-                     'ww_insurance_ids': []})
+                     'website_order_line': [],})
         rec = super(WWSaleOrderExt, self).create(vals)
         order_line = self.env['sale.order.line'].create({'order_id': rec.id,
                                                          'product_id': product.id,
                                                          'product_uom_qty': 1,
-                                                         'price_unit': 1950,
+                                                         'price_unit': vals['overnight_rate_1'] * 30,
                                                          })
         vals.update({'order_line': [(0, 0, {'id': order_line.id,
                                             'order_id': self.id})]})
 
         request.session['sale_order_id'] = rec.id
+        rec.state = 'sent'
         return rec
---
+++ wwwizard/models/ww_insurance_wizard.py
@@ -0,0 +1,52 @@
+from odoo import api, models, fields
+from odoo.http import request
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DATE_FORMAT
+from datetime import date, datetime
+
+
+class WWInsuranceWizard(models.TransientModel):
+    _name = "ww.insurance.wizard"
+
+    start_date = fields.Date()
+    child_birth_1 = fields.Date(string="Geburtsdatum")
+    overnight_rate_1 = fields.Float()
+    
+    def create(self, vals):
+        if not vals:
+            vals = {}
+
+        insurance = self.env['ww.insurance'].create({
+            'start_date': vals['start_date']
+        })
+
+        child_1 = False
+        if vals['child_birth_1']:
+            child_1 = self.env['ww.child'].create({
+                'insurance_id': insurance.id,
+                'birthday': datetime.strptime(vals['child_birth_1'],"%d.%m.%Y").date(),
+                'overnight': vals['overnight_rate_1']
+            })
+
+        if child_1:
+            insurance.write({
+                'child_ids': [(4, child_1.id)]
+            })
+
+        vals.update({
+            'ww_insurance_id': insurance.id,
+            'partner_id': self.env.user.id
+        })
+
+        sale_order = self.env['sale.order'].create(vals)
+
+        insurance.write({
+            'sale_order_id': sale_order.id
+        })
+        request.session['insurance_id'] = insurance.id
+        request.session['overnight_1'] = vals['overnight_rate_1']
+
+        signature_template = self.env['signature.request.template'].browse(8)
+        signature_template_copy = signature_template.copy()
+
+        request.session['link'] = signature_template_copy.share(signature_template_copy.id)
+        return super(WWInsuranceWizard, self).create(vals)
